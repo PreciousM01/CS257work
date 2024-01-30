f6ed40459fd15402111d7cea2fe44db837025a91
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-01-30 00:04:55 -0600

    Create cityquery.py
    Written query to check for Northfield's location

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+def get_location():
+  conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="feutsopm",
+    user="feutsopm",
+    password="java255expo")
+
+  curr = conn.cursor()
+  city_name = 'northfield'
+  cur.execute("SELECT lat, lon FROM cities WHERE city = ?",(city_name))
+  result = cur.fetchone()
+  conn.commit()
+  conn.close()
+  return result or False
+
+  location = get_location()
+  if location:
+    latitude, longitude = location
+    print(f'The location of {city_name} is latitude: {latitude} and longitude: {longitude}')
+  else:
+    print("{city_name} is not found in the database")
+  
+def main():
+    get_location()
+    
+if __name__ =="__main__":
+    main()
