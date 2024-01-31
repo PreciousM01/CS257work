b61c295da602905912cc4550b13066fb90fa4e92
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-01-31 09:17:01 -0600

    Update statequery2.sql
    creating a new view with the code title modified to id

--- us-state-pop/statequery2.sql
+++ us-state-pop/statequery2.sql
@@ -1,3 +1,5 @@
 SELECT code, state_name, population FROM states2 JOIN states ON code = abbreviation;
 CREATE VIEW state_view AS 
   SELECT code, state_name, population FROM states2 JOIN states ON code = abbreviation;
+CREATE VIEW new_state_view AS 
+  SELECT code AS id FROM state_view;
