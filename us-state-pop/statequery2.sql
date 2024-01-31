9099dfa2f81dcd4fcda72d99d64ad4ba944730cc
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-01-31 09:10:34 -0600

    Update statequery2.sql
    created view for the states

--- us-state-pop/statequery2.sql
+++ us-state-pop/statequery2.sql
@@ -1,1 +1,3 @@
 SELECT code, state_name, population FROM states2 JOIN states ON code = abbreviation;
+CREATE VIEW state_view AS 
+  SELECT code, state_name, population FROM states2 JOIN states ON code = abbreviation;
