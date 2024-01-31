7475a12b0a996cad007aa0a295039da765062965
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-01-30 19:40:24 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -48,11 +48,47 @@ def smallest_sate_city():
   result = curr.fetchone()
   smallest_statecity = result[0]
   if result:
-    print("The city with the smallest population in Minnesota is: ", smallest_statecity)
+    print("The city with the smallest population in Minnesota is:", smallest_statecity)
 
   conn.commit()
   conn.close()
-    
+
+def get_extreme_cities():
+  conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="feutsopm",
+    user="feutsopm",
+    password="java255expo")
+
+  curr= conn.cursor()
+  sql = 'SELECT city, lat, lon FROM cities'
+  curr.execute(sql)
+  cities = curr.fetchall()
+
+  furthest_west = None
+  furthest_east = None
+  furthest_north = None
+  furthest_south = None
+
+  for city in cities:
+    name, latitude, longitude = city
+    if furthest_west == None or longitude < furthest_west[1]:
+      further_west = (name, longitude)
+    if further_east == None or longitude > further_east[1]:
+      further_east = (name, longitude)
+    if further_north == None or latitude > further_north[1]:
+      further_north = (name, latitude)
+    if further_south == None or latitude < further_south[1]:
+      further_south = (name, latitude)
+
+  print("The city furthest to the West is:", further_west)
+  print("The city furthest to the East is:", further_east)
+  print("The city furthest to the North is:", further_north)
+  print("The city furthest to the South is:", further_south)
+  
+  conn.commit()
+  conn.close
   
 def main():
   city_name = 'Northfield'
@@ -65,6 +101,7 @@ def main():
 
   largest_pop()
   smallest_sate_city()
+  get_extreme_cities()
     
 if __name__ =="__main__":
     main()
