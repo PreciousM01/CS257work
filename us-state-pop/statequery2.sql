55583c055842c6ea662f5e7b09a2a93a2be99a5c
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-01-31 09:22:47 -0600

    Update statequery2.sql

--- us-state-pop/statequery2.sql
+++ us-state-pop/statequery2.sql
@@ -3,4 +3,4 @@ CREATE VIEW state_view AS
   SELECT code, state_name, population FROM states2 JOIN states ON code = abbreviation;
 CREATE VIEW new_state_view AS 
   SELECT code AS id FROM state_view;
-SELECT name, CAST(population AS REAL) AS realpop FROM states2;
+SELECT code, CAST(population AS REAL) AS realpop FROM states2;
