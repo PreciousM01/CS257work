049134979ef643edccd4df60cbdee4476413d2ce
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-02-07 09:14:07 -0600

    Update flask-test.py
    creating function to add two numbers

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -9,12 +9,16 @@ def my_function():
 
 @app.route('/display/<word1>/<word2>')
 def my_display(word1, word2):
-    the_string = "The words are: " + word1 + " and " + word2;
+    the_string = "The words are: " + word1 + " and " + word2
     return the_string
 
 @app.route('/color/<word1>')
 def my_color(word1):
     return '<h1 style="color:teal">' + word1 + '</h1>'
+
+@app.route('/add/<number1>/<number2>')
+def my_addition(number1, number2):
+    return number1 + number2
 
 if __name__ == '__main__':
     my_port = 5125
