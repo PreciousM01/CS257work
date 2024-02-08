88e55e02dbdc8dc45dccdbe3372191bfc092e66d
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-02-07 23:25:42 -0600

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -22,19 +22,17 @@ def my_addition(number1, number2):
     sum = int(number1) + int(number2)
     return str(sum)
 
-def connection_info():
-  conn = psycopg2.connect(
+    
+@app.route('/pop/<abbr>')
+def get_statepop(abbr):
+    conn = psycopg2.connect(
     host="localhost",
     port=5432,
     database="feutsopm",
     user="feutsopm",
     password="java255expo"
-  )
-  return conn
+    )
     
-@app.route('/pop/<abbr>')
-def get_statepop(abbr):
-    conn = conection_info()
     curr = conn.cursor()
     sql= 'SELECT state_population FROM states2 WHERE code = %s'
     curr.execute(sql, (abbr))
