dd15fd1594318e058ae51e491f322140234875ef
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-02-07 23:28:23 -0600

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -36,7 +36,7 @@ def get_statepop(abbr):
     curr = conn.cursor()
     sql= 'SELECT state_population FROM states2 WHERE code = %s'
     curr.execute(sql, (abbr))
-    statepop = curr.fetchone()
+    statepop = str(curr.fetchone())
     print("The population of " + abbr + "is: ", statepop)
     conn.commit()
     conn.close()
