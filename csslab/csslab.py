40f899a392cf8c610315be068cda1e027bc792cb
PreciousM01 <155037755+PreciousM01@users.noreply.github.com>
2024-02-20 17:12:23 -0600

    Update csslab.py

--- csslab/csslab.py
+++ csslab/csslab.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def load_homepage():
   return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+  my_port = 5125
+  app.run(host='0.0.0.0', port = my_port)
